292f275d692b4aa8413c8610e811151d08c84cfe
Marina Trizna <marina.tna@yandex.ru>
2024-04-29 18:13:46 +0300

    checkbox table column

--- src/app/pages/schedule/schedule.views.ts
+++ src/app/pages/schedule/schedule.views.ts
@@ -1,40 +1,56 @@
-import { CustomInputFilter } from "src/app/modules/table/filters/custom.input/custom.input";
 import {ViewInterface} from "../../interfaces/view.interface";
 import {
   AngularGridInstance,
-  Column, ExcelExportService,
-  FieldType,
-  Filters, FlatpickrOption,
-  Formatters,
-  GridOption, GridStateChange, Metrics,
-  MultipleSelectOption,
-  OperatorType
+  Column, FieldType, Formatter,
+  FormatterResultWithHtml,
+  FormatterResultWithText,
+  GridOption,
+  GridStateChange,
+  Metrics,
+  SlickGrid
 } from "angular-slickgrid";
 import {HttpClient} from "@angular/common/http";
 import {TranslateService} from "@ngx-translate/core";
 import {Injectable} from "@angular/core";
+import {CustomInputFilter} from "../../modules/table/filters/custom.input/custom.input";
 
-
+interface DataItem {
+  id: number;
+  title: string;
+  duration: string;
+  description: string;
+  percentComplete: number;
+  percentComplete2: number;
+  start: Date;
+  finish: Date;
+  effortDriven: boolean;
+  phone: string;
+  completed: number;
+}
 function randomBetween(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 const NB_ITEMS = 1500;
 const URL_SAMPLE_COLLECTION_DATA = 'assets/data/collection_500_numbers.json';
 
+
 @Injectable({ providedIn: 'root' })
 export class ScheduleViews implements ViewInterface {
+  [x: string]: any;
   angularGrid!: AngularGridInstance;
   metrics!: Metrics;
   constructor(private http: HttpClient, private translate: TranslateService) { }
-  getTableColumns(): Column[] {
 
-    // @ts-ignore
+
+  getTableColumns(): Column<DataItem>[] {
     return [
-
-
       {
-        id: 'title', name: '', field: 'title', sortable: true, minWidth: 10,
-        type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
+        id: 'completed', name: 'Completed', field: 'completed', type: FieldType.number, sortable: true, minWidth: 20,
+        maxWidth: 200,
+        formatter: customEnableButtonFormatter,
+        onCellClick: (e, args) => {
+          this.toggleCompletedProperty(args && args.dataContext);
+        }
       },
       {
         id: 'description', name: 'ID Пациента', field: 'description', filterable: true, sortable: true, minWidth: 80,
@@ -44,134 +60,23 @@ export class ScheduleViews implements ViewInterface {
           enableTrimWhiteSpace: true // or use global "enableFilterTrimWhiteSpace" to trim on all Filters
         }
       },
-      {
-        id: 'duration', name: 'Фамилия И.О.', field: 'duration', sortable: true, type: FieldType.number, exportCsvForceToKeepAsString: true,
-        minWidth: 55,
-        filterable: true,
-        filter: {
-          collectionAsync: this.http.get<{ option: string; value: string; }[]>(URL_SAMPLE_COLLECTION_DATA),
-          // collectionFilterBy & collectionSortBy accept a single or multiple options
-          // we can exclude certains values 365 & 360 from the dropdown filter
-          collectionFilterBy: [{
-            property: 'value',
-            operator: OperatorType.notEqual,
-            value: 360
-          }, {
-            property: 'value',
-            operator: OperatorType.notEqual,
-            value: 365
-          }],
-          collectionSortBy: {
-            property: 'value',
-            sortDesc: true,
-            fieldType: FieldType.number
-          },
-          customStructure: {
-            value: 'value',
-            label: 'label',
-            optionLabel: 'value', // if selected text is too long, we can use option labels instead
-            labelSuffix: 'text',
-          },
-          collectionOptions: {
-            separatorBetweenTextLabels: ' ',
-            filterResultAfterEachPass: 'chain' // options are "merge" or "chain" (defaults to "chain")
-          },
-          model: Filters.multipleSelect,
-
-          // we could add certain option(s) to the "multiple-select" plugin
-          filterOptions: {
-            maxHeight: 250,
-            width: 175,
-
-            // if we want to display shorter text as the selected text (on the select filter itself, parent element)
-            // we can use "useSelectOptionLabel" or "useSelectOptionLabelToHtml" the latter will parse html
-            useSelectOptionLabelToHtml: true
-          } as MultipleSelectOption
-        }
-      },
-      {
-        id: 'complete', name: 'Дата рождения', field: 'percentComplete', formatter: Formatters.percentCompleteBar, minWidth: 70, type: FieldType.number, sortable: true,
-        filterable: true, filter: { model: Filters.compoundInputNumber }
-      },
-      {
-        id: 'Услуга (исследование)', name: 'Услуга (исследование)', field: 'start', formatter: Formatters.dateIso, sortable: true, minWidth: 75,
-        type: FieldType.date, filterable: true, filter: { model: Filters.compoundDate }
-      },
-      {
-        id: 'usDateShort', name: 'Запись', field: 'usDateShort', sortable: true, minWidth: 70, width: 70,
-        type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      },
-      // {
-      //   id: 'utcDate', name: 'UTC Date', field: 'utcDate', formatter: Formatters.dateTimeIsoAmPm, sortable: true, minWidth: 115,
-      //   type: FieldType.dateUtc, exportWithFormatter: true, outputType: FieldType.dateTimeIsoAmPm, filterable: true,
-      //   filter: {
-      //     model: Filters.compoundDate,
-      //     // override any of the Flatpickr options through "filterOptions"
-      //     // please note that there's no TSlint on this property since it's generic for any filter, so make sure you entered the correct filter option(s)
-      //     filterOptions: { minDate: 'today' } as FlatpickrOption
-      //   }
-      // },
-      {
-        id: 'effort-driven', name: 'Cito!', field: 'effortDriven.isEffort', minWidth: 85, maxWidth: 85,
-        type: FieldType.boolean,
-        sortable: true,
-        exportCustomFormatter: Formatters.complexObject,
-
-        // to pass multiple formatters, use the params property
-        // also these formatters are executed in sequence, so if you want the checkmark to work correctly, it has to be the last formatter defined
-        formatter: Formatters.multiple,
-        params: { formatters: [Formatters.complexObject, Formatters.checkmark] },
-
-        // when the "field" string includes the dot "." notation, the library will consider this to be a complex object and Filter accordingly
-        filterable: true,
-        filter: {
-          // We can also add HTML text to be rendered (any bad script will be sanitized) but we have to opt-in, else it will be sanitized
-          // enableRenderHtml: true,
-          // collection: [{ value: '', label: '' }, { value: true, label: 'True', labelPrefix: `<i class="fa fa-check"></i> ` }, { value: false, label: 'False' }],
-
-          collection: [{ isEffort: '', label: '' }, { isEffort: true, label: 'True' }, { isEffort: false, label: 'False' }],
-          customStructure: {
-            value: 'isEffort',
-            label: 'label'
-          },
-          model: Filters.singleSelect,
-
-          // we could add certain option(s) to the "multiple-select" plugin
-          filterOptions: { autoAdjustDropHeight: true } as MultipleSelectOption,
-        }
-
-      },
-      // {
-      //   id: 'title', name: 'Услуга (исследование)', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      //
-      // {
-      //   id: 'title', name: 'Статус', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      // {
-      //   id: 'title', name: 'Тара', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      // {
-      //   id: 'title', name: 'Отклонение', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      // {
-      //   id: 'title', name: 'Штрихкод', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      // {
-      //   id: 'title', name: 'Пункт забора', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // },
-      // {
-      //   id: 'title', name: 'Дата направления', field: 'title', sortable: true, minWidth: 55,
-      //   type: FieldType.string, filterable: true, filter: { model: Filters.compoundInputText }
-      // }
     ];
+
+
   }
+
+  toggleCompletedProperty(item: any) {
+    // toggle property
+    if (typeof item === 'object') {
+      item.completed = !item.completed;
+
+      // simulate a backend http call and refresh the grid row after delay
+      setTimeout(() => {
+        this.angularGrid.gridService.updateItemById(item.id, item, {highlightRow: false});
+      }, 250);
+    }
+  }
+
 
   getTableOptions(): GridOption {
 
@@ -299,3 +204,16 @@ export class ScheduleViews implements ViewInterface {
     this.angularGrid.slickGrid.navigateTop();
   }
 }
+
+
+const customEnableButtonFormatter: Formatter<DataItem> = (_row: number, _cell: number, value: any) => {
+  return `<span style="margin-left: 5px">
+      <button class="btn btn-xs btn-default">
+        <i class="fa ${value ? 'fa-check-circle' : 'fa-circle-thin'} fa-lg" style="color: ${value ? 'black' : 'lavender'}"></i>
+      </button>
+    </span>`;
+};
+function toggleCompletedProperty(item: any, any: any) {
+  throw new Error("Function not implemented.");
+}
+
