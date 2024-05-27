0d268b141fcdb2bdb58641f041a4d19b91a4b41f
Marina Trizna <marina.tna@yandex.ru>
2024-05-27 13:34:36 +0300

    1

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { MainPageRoutingModule } from './main-routing.module';
 
 import { MainPage } from './main.page';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MainPageRoutingModule
-  ],
+    MainPageRoutingModule,
+    RouterModule.forChild([{ path: '', component: MainPage }])],
+
   declarations: [MainPage]
 })
+
+
 export class MainPageModule {}
--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -12,4 +12,6 @@ export class MainPage implements OnInit {
   ngOnInit() {
   }
 
+
+
 }
