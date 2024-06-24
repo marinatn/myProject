4931048bfd89fcf58e1aff9299b6d746010cf6cb
Sergei Trizna <init.reg@gmail.com>
2024-06-25 01:24:14 +0300

    dev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {IonMenu} from "@ionic/angular/directives/proxies";
 
 // export const APP_API_URL = "http://localhost:8000/api";
 export const APP_API_URL = "http://45.141.100.40/api"
+
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
@@ -14,42 +15,21 @@ export const APP_API_URL = "http://45.141.100.40/api"
   styleUrls: [
     'app.component.scss'
   ],
-
-
-  // imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 
 
-
 export class AppComponent {
-  private values: string [] = ['1', '2'];
-  public activeTab: string = 'test';
   @ViewChild('mainMenu') mainMenu: IonMenu | undefined;
-  constructor(private router: Router, protected authService: AuthenticationService) {}
-
-  accordionGroupChange = (ev: any) => {
-    const collapsedItems = this.values.filter((value) => value !== ev.detail.value);
-    const selectedValue = ev.detail.value;
-
-    console.log(
-      `Expanded: ${selectedValue === undefined ? 'None' : ev.detail.value} | Collapsed: ${collapsedItems.join(', ')}`
-    );
-  }
-
-  async change(activeTab: string) {
-    this.activeTab =  activeTab;
-    await this.router.navigate([activeTab]);
-  }
-
   protected readonly APP_ROUTES = APP_ROUTES;
 
+  constructor(private router: Router, protected authService: AuthenticationService) {
+  }
+
   async goToPage(ref: string) {
-    await this.router.navigate([ref]);
+    await this.router.navigate([ref], {replaceUrl: true});
     if (this.mainMenu) {
       await this.mainMenu.close(true);
     }
-
-
   }
 }
 
